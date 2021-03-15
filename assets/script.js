79e1330607bae77ba5e7f90d4db5bd33908596ad
delta-kor <deltakbot1@gmail.com>
2021-03-15 13:52:53 +0900

    Fix cursor position

--- assets/script.js
+++ assets/script.js
@@ -4,8 +4,8 @@ window.addEventListener('mousemove', e => {
   const { clientWidth: width, clientHeight: height } = document.body;
   const { clientX: x, clientY: y } = e;
 
-  const deltaX = (x / width - 0.5) * 3;
-  const deltaY = (y / height - 0.5) * 3;
+  const deltaX = (x / width - 0.5) * -3;
+  const deltaY = (y / height - 0.5) * -3;
 
   landingCover.style.left = `${50 + deltaX}%`;
   landingCover.style.top = `${50 + deltaY}%`;
