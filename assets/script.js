334edab5f5f16f61d58408b2399a96e0b42cbc8f
delta-kor <deltakbot1@gmail.com>
2021-03-15 15:10:30 +0900

    Add ui class

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,33 @@
-const landingCover = document.querySelector('.landing > .cover');
+class UI {
+  static followCursor(x, y) {
+    const landingCover = document.querySelector('.landing > .cover');
+    const { clientWidth: width, clientHeight: height } = document.body;
+
+    const deltaX = (x / width - 0.5) * -3;
+    const deltaY = (y / height - 0.5) * -3;
+
+    landingCover.style.left = `${50 + deltaX}%`;
+    landingCover.style.top = `${50 + deltaY}%`;
+  }
+
+  static highlight(state) {
+    const highlight = document.querySelector('.landing > .description > span.highlight');
+    state ? highlight.classList.add('active') : highlight.classList.remove('active');
+  }
+}
 
 window.addEventListener('mousemove', e => {
-  const { clientWidth: width, clientHeight: height } = document.body;
   const { clientX: x, clientY: y } = e;
-
-  const deltaX = (x / width - 0.5) * -3;
-  const deltaY = (y / height - 0.5) * -3;
-
-  landingCover.style.left = `${50 + deltaX}%`;
-  landingCover.style.top = `${50 + deltaY}%`;
+  UI.followCursor(x, y);
 });
+
+async function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function land() {
+  await delay(500);
+  UI.highlight(true);
+}
+
+void land();
