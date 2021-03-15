8bcc685fbceefda0edd72e8b88322f347ee94828
delta-kor <deltakbot1@gmail.com>
2021-03-15 16:22:10 +0900

    Add opening

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,10 @@
+let currentSection = 0;
+const audio = new Audio();
+
 class UI {
   static followCursor(x, y) {
     const landingCover = document.querySelector('.landing > .cover');
-    const { clientWidth: width, clientHeight: height } = document.body;
+    const { innerWidth: width, innerHeight: height } = window;
 
     const deltaX = (x / width - 0.5) * -3;
     const deltaY = (y / height - 0.5) * -3;
@@ -14,12 +17,39 @@ class UI {
     const highlight = document.querySelector('.landing > .description > span.highlight');
     state ? highlight.classList.add('active') : highlight.classList.remove('active');
   }
-}
 
-window.addEventListener('mousemove', e => {
-  const { clientX: x, clientY: y } = e;
-  UI.followCursor(x, y);
-});
+  static scroll() {
+    if (currentSection === 0) {
+      window.scrollTo(0, window.innerHeight);
+      void UI.title(0);
+      return true;
+    }
+  }
+
+  static async title(index) {
+    const lines = document.querySelectorAll('.fixed > .content > .line');
+    const highlight = document.querySelector('.fixed > .content > .line > .highlight');
+
+    if (index === 0) {
+      audio.src = 'music/colors.mp3';
+
+      await audio.play();
+      audio.currentTime = 1;
+
+      await delay(800);
+      lines[0].classList.add('active');
+
+      await delay(4800);
+      lines[1].classList.add('active');
+
+      await delay(1350);
+      highlight.classList.add('active');
+
+      await delay(100);
+      document.querySelector('.section > .cover').classList.add('active');
+    }
+  }
+}
 
 async function delay(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -31,3 +61,14 @@ async function land() {
 }
 
 void land();
+
+window.addEventListener('mousemove', e => {
+  const { clientX: x, clientY: y } = e;
+  UI.followCursor(x, y);
+});
+
+for (const scroll of document.getElementsByClassName('scroll')) {
+  scroll.addEventListener('click', () => {
+    UI.scroll();
+  });
+}
