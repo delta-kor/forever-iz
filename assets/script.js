be5c1a0f2f4895ab07cb6f4f1ada7fb0b6480bcb
delta-kor <deltakbot1@gmail.com>
2021-03-15 13:43:35 +0900

    Add cursor tracking

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+const landingCover = document.querySelector('.landing > .cover');
+
+window.addEventListener('mousemove', e => {
+  const { clientWidth: width, clientHeight: height } = document.body;
+  const { clientX: x, clientY: y } = e;
+
+  const deltaX = (x / width - 0.5) * 3;
+  const deltaY = (y / height - 0.5) * 3;
+
+  landingCover.style.left = `${50 + deltaX}%`;
+  landingCover.style.top = `${50 + deltaY}%`;
+});
