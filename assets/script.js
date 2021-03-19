c4b04eee5db283287ef05ecd9c79b3919b62c9de
delta-kor <deltakbot1@gmail.com>
2021-03-19 16:29:12 +0900

    Fix anchor

--- assets/script.js
+++ assets/script.js
@@ -9,7 +9,7 @@ const search = new URLSearchParams(location.search);
 const isEnding = search.has('ending');
 
 const version = document.querySelector('.startup > .version');
-version.textContent = 'vc53-210318';
+version.textContent = 'vc54-210319';
 
 if (isEnding) version.textContent += ' (#ending)';
 
