cf5998f43385a9e08ddeaf6479d121e0d35a3426
delta-kor <deltakbot1@gmail.com>
2021-03-19 17:22:02 +0900

    Add pwa

---
+++ sw.js
@@ -0,0 +1,30 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('./sw.js', { scope: './' }).then(registration => {
+      console.log(registration);
+    });
+  });
+}
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('cache').then(cache => {
+      return cache.addAll(['./', './assets/src/script.js', './assets/src/style.css']);
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    (async function () {
+      try {
+        const response = await fetch(event.request);
+        const cache = await caches.open('cache');
+        cache.put(event.request.url, response.clone());
+        return response;
+      } catch (error) {
+        return caches.match(event.request);
+      }
+    })()
+  );
+});
